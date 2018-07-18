c9a4e7adcea4935350580893cd4aa2648ab9e3ff
Ezequiel R. Aguerre <ezequiel@amalgama.co>
2018-07-18 13:03:22 -0300

    Allow user to input anything
    This is to avoid handling special cases (copy, paste, arrows, etc).

--- hamming.js
+++ hamming.js
@@ -6,22 +6,8 @@
 
 		boton.addEventListener( 'click', calcularYMostrarCodigHamming );
 		texto.addEventListener( 'keydown', function(ev) {
-			if (
-					ev.key.startsWith( "Arrow" ) ||
-					ev.key === "Home" ||
-					ev.key === "End" ||
-					ev.key === "Backspace" ||
-					ev.key === "Delete" 
-				)
-				return;
-
-			if ( ev.key === "0" || ev.key === "1" )
-				return;
-
-			if ( ev.key === "Enter" )
+			if ( ev.key === "Enter" || ev.key === "Accept" )
 				calcularYMostrarCodigHamming();
-
-			ev.preventDefault();
 		} );
 
 		function esPotenciaDeDos(n) {
@@ -39,14 +25,13 @@
 			var bits = textoEntrada.length;
 			var textoSalida = [];
 			var textoParidad = '';
-			var tamanoPalabra = 0;
-			var indiceEntrada = 1;
+			var bitNumero = 1;
 			var indice = 0;
 			var bitsDeParidad = 0;
 
 			// Agregar bits de paridad
 			while ( indice < bits ) {
-				if ( esPotenciaDeDos( indiceEntrada ) ) {
+				if ( esPotenciaDeDos( bitNumero ) ) {
 					textoSalida.push( 'P' );
 					bitsDeParidad++;
 				} else {
@@ -54,18 +39,18 @@
 					indice++;
 				}
 
-				indiceEntrada++;
+				bitNumero++;
 			}
 
 			// Calcular los bits de paridad
 			for ( indice = 0; indice < textoSalida.length; indice++ ) {
 				if ( textoSalida[indice] == 'P' ) {
-					var nro = indice + 1;
+					bitNumero = indice + 1;
 					var paridad = 0;
 					var j;
 
 					for ( j = 0; j < textoSalida.length; j++ ) {
-						if ( j != indice && ((j+1) & nro) !== 0 ) {
+						if ( j != indice && ((j+1) & bitNumero) !== 0 ) {
 							if ( textoSalida[j] == '1' )
 								paridad++;
 						}
